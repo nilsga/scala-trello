0e2496796d8f6b98fd920fdc9807b417e774dec5
Nils-Helge Garli Hegvik <nilsga@gmail.com>
2015-10-11 12:33:47 +0200

    Switched to wordspec

--- src/test/scala/com/github/nilsga/trello/TrelloApiIntegrationTest.scala
+++ src/test/scala/com/github/nilsga/trello/TrelloApiIntegrationTest.scala
@@ -2,12 +2,12 @@ package com.github.nilsga.trello
 
 import akka.actor.ActorSystem
 import com.typesafe.config.ConfigFactory
-import org.scalatest.{Matchers, FlatSpec}
+import org.scalatest.{Matchers, WordSpec}
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
-class TrelloApiIntegrationTest extends FlatSpec with Matchers {
+class TrelloApiIntegrationTest extends WordSpec with Matchers {
 
   val config = ConfigFactory.load()
   implicit val actorSystem = ActorSystem("TrelloApiIntegrationTests", config)
@@ -15,25 +15,28 @@ class TrelloApiIntegrationTest extends FlatSpec with Matchers {
 
   val api = TrelloApi(config.getString("trello.key"), config.getString("trello.token"))
 
-  "Trello API" should "Get member" in {
-    val member = Await.result(api.member("me"), 10 seconds)
-    member.fullName should be("Trello Async Client")
-  }
+  "Trello API" should {
 
-  "Trello API" should "Get board" in {
-    val board = Await.result(api.board("5618cf5e0c80b6fd6b667971"), 10 seconds)
-    board.name should be("Trello Integration Tests")
-  }
+    "Get member" in {
+      val member = Await.result(api.member("me"), 10 seconds)
+      member.fullName should be("Trello Async Client")
+    }
 
-  "Trello API" should "Get cards for board" in {
-    val cards = Await.result(api.cards("5618cf5e0c80b6fd6b667971"), 10 seconds)
-    cards.head.desc should be("Test card description")
-    println(cards)
-  }
+    "Get board" in {
+      val board = Await.result(api.board("5618cf5e0c80b6fd6b667971"), 10 seconds)
+      board.name should be("Trello Integration Tests")
+    }
 
-  "Trello API" should "Get card" in {
-    val card = Await.result(api.card("5618cf67739728febefa2980"), 10 seconds)
-    card.desc should be("Test card description")
-    println(card)
+    "Get cards for board" in {
+      val cards = Await.result(api.cards("5618cf5e0c80b6fd6b667971"), 10 seconds)
+      cards.head.desc should be("Test card description")
+      println(cards)
+    }
+
+    "Get card" in {
+      val card = Await.result(api.card("5618cf67739728febefa2980"), 10 seconds)
+      card.desc should be("Test card description")
+      println(card)
+    }
   }
 }
